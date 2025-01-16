68cec12170c3b76d7c615dbaddcbe13e96fdf8e1
F4ris5 <mrfaris2011@gmail.com>
2025-01-16 10:14:39 +0800

    Updated address containerization

--- cors-server/server.js
+++ cors-server/server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://food-order-app:5000', // Restrict to specific origin
+  })
+);
 
 // Set up multer for file uploads
 const storage = multer.memoryStorage();
@@ -40,6 +44,6 @@ app.post('/upload-image', upload.single('source'), async (req, res) => {
   }
 });
 
-app.listen(PORT, 'food-order-app', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
